cae8e747983757e9392a1d9f03775b3c1f7d1cc2
Diego Hernandez <Diego.Hernandez@outsell.com>
2022-09-28 16:33:45 -0500

    added form ended at form

--- WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -13,7 +13,7 @@ namespace WiredBrainCoffeeAdmin.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        public List<SurveyItem> SurveyResults { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -21,7 +21,9 @@ namespace WiredBrainCoffeeAdmin.Pages
 
         public void OnGet()
         {
-
+            var rawJson = System.IO.File
+                .ReadAllText("wwwroot/sampledata/survey.json");
+            SurveyResults = JsonSerializer.Deserialize<List<SurveyItem>>(rawJson);
         }
     }
 }
--- WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Products/AddProduct.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffeeAdmin.Data;
 
 namespace WiredBrainCoffeeAdmin.Pages.Products
 {
     public class AddProductModel : PageModel
     {
+        [BindProperty]
+       public Product NewProdcut { get; set; }
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {
+            // Save product to database
+            var productName = NewProdcut.Name;
+           
         }
     }
 }
